f54f902236a6246890987201c3028b6968e31219
cyr <cyril.quandalle@chanel.com>
2020-03-11 15:00:12 +0100

    add pristineState, setErrorField, isFormEmpty, isFormInError

--- src/useForm.js
+++ src/useForm.js
@@ -4,6 +4,7 @@ export const useForm = (stateSchema, submit) => {
   const [state, setState] = useState(stateSchema)
   const [formInError, setFormInError] = useState(false)
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [pristineState, setPristineState] = useState({})
 
   const handleChange = ({ target: { value, name, type, checked } }) => {
     value = type === 'checkbox' ? checked : value
@@ -20,11 +21,48 @@ export const useForm = (stateSchema, submit) => {
     }))
   }
 
+  const setErrorField = (error, name) => {
+    setState(prevState => ({
+      ...prevState,
+      [name]: { ...state[name], error },
+    }))
+  }
+
   const onValidate = ({ target: { name } }) => {
     setState(prevState => ({
       ...prevState,
       [name]: { ...state[name], error: getErrorMessage(state, name) },
     }))
+  }
+
+  const isFormEmpty = required => {
+    let isEmpty = true
+    for (const field in state) {
+      if (required) {
+        if (state[field].required && state[field].value) {
+          isEmpty = false
+        }
+      } else {
+        if (state[field].value) {
+          isEmpty = false
+        }
+      }
+    }
+    return isEmpty
+  }
+
+  const isPristine = () => {
+    for (const field in pristineState) {
+      if (pristineState[field].value !== state[field].value) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const setInitialState = state => {
+    setState(state)
+    setPristineState(state)
   }
 
   const validateAllFields = () => {
@@ -38,8 +76,18 @@ export const useForm = (stateSchema, submit) => {
     setFormInError(isInerror)
   }
 
-  const handleReset = (schema = stateSchema) => {
-    setState(schema)
+  const isFormInerror = () => {
+    let isInerror = false
+    for (const fieldName of Object.keys(stateSchema)) {
+      if (getErrorMessage(state, fieldName)) {
+        isInerror = true
+      }
+    }
+    return isInerror
+  }
+
+  const handleReset = schema => {
+    setState(schema || stateSchema)
     setFormInError(false)
     setIsSubmitting(false)
   }
@@ -56,7 +104,9 @@ export const useForm = (stateSchema, submit) => {
         try {
           await submit()
         } catch (e) {
-          console.error('An error occured during form validation')
+          console.error(
+            "Une erreur s'est produite lors de la soumission du formulaire"
+          )
         } finally {
           setIsSubmitting(false)
         }
@@ -69,10 +119,17 @@ export const useForm = (stateSchema, submit) => {
     state,
     handleChange,
     setFormField,
+    setErrorField,
     onValidate,
     handleSubmit,
     handleReset,
+    isFormEmpty,
     validateAllFields,
+    formInError,
+    setState,
+    isFormInerror,
+    setInitialState,
+    isPristine,
   }
 }
 
@@ -81,7 +138,7 @@ const getErrorMessage = (state, name) => {
     if (state[name].requiredError) {
       return state[name].requiredError
     }
-    return 'Ce champ est obligatoire'
+    return 'Champ requis'
   }
 
   if (state[name].required && state[name].value) {
