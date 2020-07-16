3f23520af12b405417964c461415c7b651c4c484
cyr <cyril.quandalle@gmail.com>
2020-07-16 13:27:25 +0200

    add test for field validation

--- src/useForm.spec.js
+++ src/useForm.spec.js
@@ -23,4 +23,106 @@ describe('useForm', () => {
 
     expect(result.current.state.myField.value).toEqual('newValue')
   })
+
+  it('should display an error if the field is required and empty', () => {
+    const { result } = renderHook(() =>
+      useForm({ myField: { value: '', required: true, error: '' } })
+    )
+    act(() => {
+      result.current.onValidate({ target: { name: 'myField' } })
+    })
+
+    expect(result.current.state.myField.error).toEqual('Champ requis')
+  })
+
+  it('should display a custom error for field with custom validation and wrong value', () => {
+    const { result } = renderHook(() =>
+      useForm({
+        myField: {
+          value: 'test@jj',
+          required: true,
+          error: '',
+          validator: {
+            regEx: /^[a-z0-9]+([|.|-]{1}[a-z0-9]+)*@[a-z0-9]+([|.|-]{1}[a-z0-9]+)*[.]{1}[a-z]{2,6}$/i,
+            error: 'invalid email'
+          }
+        }
+      })
+    )
+
+    act(() => {
+      result.current.onValidate({ target: { name: 'myField' } })
+    })
+
+    expect(result.current.state.myField.error).toEqual('invalid email')
+  })
+
+  it('should validate all fields and display their error', () => {
+    const { result } = renderHook(() =>
+      useForm({
+        myField: { value: '', required: true, error: '' },
+        email: {
+          value: 'test@jj',
+          required: true,
+          error: '',
+          validator: {
+            regEx: /^[a-z0-9]+([|.|-]{1}[a-z0-9]+)*@[a-z0-9]+([|.|-]{1}[a-z0-9]+)*[.]{1}[a-z]{2,6}$/i,
+            error: 'invalid email'
+          }
+        }
+      })
+    )
+    act(() => {
+      result.current.validateAllFields()
+    })
+
+    expect(result.current.state.myField.error).toEqual('Champ requis')
+    expect(result.current.state.email.error).toEqual('invalid email')
+  })
+
+  it('should display an error if a field which is only required if another field is filled is empty and the other is not', () => {
+    const { result } = renderHook(() =>
+      useForm({
+        myField: { value: 'ee', required: true, error: '' },
+        email: {
+          value: '',
+          required: true,
+          error: '',
+          dependsOn: 'myField',
+          validator: {
+            regEx: /^[a-z0-9]+([|.|-]{1}[a-z0-9]+)*@[a-z0-9]+([|.|-]{1}[a-z0-9]+)*[.]{1}[a-z]{2,6}$/i,
+            error: 'invalid email'
+          }
+        }
+      })
+    )
+    act(() => {
+      result.current.validateAllFields()
+    })
+
+    expect(result.current.state.email.error).toEqual('Champ requis')
+  })
+
+  it('should not display an error if a field which is only required if another field is filled is empty and the other is too', () => {
+    const { result } = renderHook(() =>
+      useForm({
+        myField: { value: '', required: true, error: '' },
+        email: {
+          value: '',
+          required: true,
+          error: '',
+          dependsOn: 'myField',
+          validator: {
+            regEx: /^[a-z0-9]+([|.|-]{1}[a-z0-9]+)*@[a-z0-9]+([|.|-]{1}[a-z0-9]+)*[.]{1}[a-z]{2,6}$/i,
+            error: 'invalid email'
+          }
+        }
+      })
+    )
+    act(() => {
+      result.current.validateAllFields()
+    })
+
+    expect(result.current.state.email.error).toEqual('')
+  })
 })
